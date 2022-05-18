cb5b026d06c9db67e5e4160583a5bf3d48e56229
deniselupe <dgrodriguez24@hotmail.com>
2022-05-18 21:27:22 +0000

    feat: introduce Tree class and build_tree() method

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -9,4 +9,24 @@ class Node
   end
 end
 
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    @array = array
+    @root = build_tree(@array, 0, @array.length - 1)
+  end
+
+  def build_tree(array, min, max)
+    return nil if min > max
+    mid = (min + max) / 2
+    node = Node.new(array[mid])
+    node.left = build_tree(array, min, mid - 1)
+    node.right = build_tree(array, mid + 1, max)
+    node
+  end
+end
+
 arr1 = [1, 2, 3, 4, 5, 6, 7, 8]
+bst = Tree.new(arr1)
+p bst.root
