aa0901f0733057d135b2b7157f585500db4a52b7
deniselupe <dgrodriguez24@hotmail.com>
2022-05-24 02:16:10 +0000

    feat: add solution for delete method

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -75,11 +75,72 @@ class Tree
     root.right = insert(value, root.right) if value > root.data
     root
   end
+
+  # when value matches node, traverse for inorder successor
+  # if node has a right subtree
+  def successor(node)
+    current_node = node
+
+    until current_node.left.nil?
+      current_node = current_node.left
+    end
+
+    current_node.data
+  end
+
+  # when value matches node, traverse for inorder predecessor
+  # if node has no right subtree but has a left subtree
+  def predecessor(node)
+    current_node = node
+
+    until current_node.right.nil?
+      current_node = current_node.right
+    end
+
+    current_node.data
+  end
+
+  def delete(value, root = @root)
+    if value < root.data
+      root.left = delete(value, root.left)
+    elsif value > root.data
+      root.right = delete(value, root.right)
+    elsif value == root.data
+      # deletes node if it has no children
+      if root.left.nil? && root.right.nil?
+        root = nil
+      # deletes inorder successor node if right subtree exists
+      elsif !root.right.nil?
+        temp = successor(root.right)
+        root.data = temp
+        root.right = delete(temp, root.right)
+      # deletes inorder predecessor if no right subtree exists
+      elsif !root.left.nil?
+        temp = predecessor(root.left)
+        root.data = temp
+        root.left = delete(temp, root.left)
+      end
+    end
+
+    root
+  end
 end
 
 arr1 = [6, 5, 3, 1, 8, 7, 2, 4, 6, 7, 1, 6, 6]
 bst = Tree.new(arr1)
 p "Array is #{bst.array}"
+puts '-------'
+puts 'Original Tree:'
 p bst.root
+puts '-------'
+puts 'Tree when attempting to insert a duplicate number such as 6:'
 p bst.insert(6)
+puts '-------'
+puts 'Tree when attempting to insert a non-duplicate number such as 9:'
 p bst.insert(9)
+puts '-------'
+puts 'Tree when deleting the recently added 9:'
+p bst.delete(9)
+puts '-------'
+puts 'Tree when removing root node 4:'
+p bst.delete(4)
